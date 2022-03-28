82eb3acd6e4d8e78c8074f8efd5a6326df8fab4e
Liuxiaowei <liu17621749212@163.com>
2022-03-29 00:21:34 +0800

    feat(handwritten): 手写mybatis

--- handwritten-mybatis/src/main/java/com/open/mybatis/v1/SqlSession.java
+++ handwritten-mybatis/src/main/java/com/open/mybatis/v1/SqlSession.java
@@ -29,5 +29,4 @@ public class SqlSession {
         return configuration.getMapper(clazz, this);
     }
 
-
 }
---
+++ handwritten-mybatis/src/main/java/com/open/mybatis/v2/SqlSessionFactory.java
@@ -0,0 +1,10 @@
+package com.open.mybatis.v2;
+
+/**
+ * @author liuxiaowei
+ * @date 2022年03月29日 0:20
+ * @Description
+ */
+public class SqlSessionFactory {
+
+}
